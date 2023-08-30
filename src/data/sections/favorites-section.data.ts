41e680f564458b1022205977cc501ba51295dfe1
CatMonster <stefano.castiglia@protonmail.ch>
2023-08-30 08:15:46 +0200

    Updates favorites section

--- src/data/sections/favorites-section.data.ts
+++ src/data/sections/favorites-section.data.ts
@@ -9,86 +9,64 @@ const favoritesSectionData = {
     visible: true,
   },
   books: {
-    title: 'Books I read',
+    title: `Books I'm reading`,
     data: [
       {
-        image: import('@/assets/favorites/books/book-1.jpeg'),
-        title: 'The Pragmatic Programmer: From Journeyman to Master',
-        author: 'Andy Hunt, Dave Thomas',
-        url: 'https://www.goodreads.com/book/show/4099.The_Pragmatic_Programmer',
+        image: import('@/assets/favorites/books/refactoring-ui.png'),
+        title: 'Refactoring UI',
+        author: 'Adam Wathan, Steve Schoger',
+        url: 'https://www.refactoringui.com/',
       },
       {
-        image: import('@/assets/favorites/books/book-2.jpg'),
-        title: 'Domain-Driven Design: Tackling Complexity in the Heart of Software',
-        author: 'Eric Evans',
-        url: 'https://www.goodreads.com/book/show/179133.Domain_Driven_Design',
-      },
-      {
-        image: import('@/assets/favorites/books/book-3.jpeg'),
-        title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
-        author: 'Robert C. Martin',
-        url: 'https://www.goodreads.com/book/show/3735293-clean-code',
-      },
-      {
-        image: import('@/assets/favorites/books/book-4.jpeg'),
-        title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
-        author: 'Robert C. Martin',
-        url: 'https://www.goodreads.com/book/show/10284614-the-clean-coder',
+        image: import('@/assets/favorites/books/typescript-50.jpg'),
+        title: 'TypeScript in 50 Lessons',
+        author: 'Stefan Baumgartner',
+        url: 'https://typescript-book.com/',
       },
     ],
   },
-  people: {
-    title: 'People I learn from',
-    data: [
-      {
-        image: import('@/assets/favorites/people/person-1.jpg'),
-        name: 'Kent C. Dodds',
-        url: 'https://kentcdodds.com/',
-      },
-      {
-        image: import('@/assets/favorites/people/person-2.jpeg'),
-        name: 'Kent Beck',
-        url: 'https://www.kentbeck.com/',
-      },
-      {
-        image: import('@/assets/favorites/people/person-3.jpeg'),
-        name: 'Eric Evans',
-        url: 'https://www.domainlanguage.com/',
-      },
-      {
-        image: import('@/assets/favorites/people/person-4.jpeg'),
-        name: 'Martin Fowler',
-        url: 'https://martinfowler.com/',
-      },
-      {
-        image: import('@/assets/favorites/people/person-5.jpg'),
-        name: 'Robert C. Martin',
-        url: 'http://cleancoder.com/',
-      },
-      {
-        image: import('@/assets/favorites/people/person-6.jpeg'),
-        name: 'Adam Dymitruk',
-        url: 'https://eventmodeling.org/',
-      },
-    ],
-  },
+  // people: {
+  //   title: 'People I learn from',
+  //   data: [
+  //     {
+  //       image: import('@/assets/favorites/people/person-1.jpg'),
+  //       name: 'Kent C. Dodds',
+  //       url: 'https://kentcdodds.com/',
+  //     },
+  //     {
+  //       image: import('@/assets/favorites/people/person-2.jpeg'),
+  //       name: 'Kent Beck',
+  //       url: 'https://www.kentbeck.com/',
+  //     },
+  //     {
+  //       image: import('@/assets/favorites/people/person-3.jpeg'),
+  //       name: 'Eric Evans',
+  //       url: 'https://www.domainlanguage.com/',
+  //     },
+  //     {
+  //       image: import('@/assets/favorites/people/person-4.jpeg'),
+  //       name: 'Martin Fowler',
+  //       url: 'https://martinfowler.com/',
+  //     },
+  //     {
+  //       image: import('@/assets/favorites/people/person-5.jpg'),
+  //       name: 'Robert C. Martin',
+  //       url: 'http://cleancoder.com/',
+  //     },
+  //     {
+  //       image: import('@/assets/favorites/people/person-6.jpeg'),
+  //       name: 'Adam Dymitruk',
+  //       url: 'https://eventmodeling.org/',
+  //     },
+  //   ],
+  // },
   videos: {
-    title: 'Videos I watched',
+    title: 'Courses i followed',
     data: [
       {
-        image: import('@/assets/favorites/videos/video-1.jpeg'),
-        title: 'Building Resilient Frontend Architecture • Monica Lent • GOTO 2019',
-        url: 'https://youtu.be/TqfbAXCCVwE',
-      },
-      {
-        image: import('@/assets/favorites/videos/video-2.jpeg'),
-        title: 'Scaling Yourself • Scott Hanselman • GOTO 2012',
-        url: 'https://youtu.be/FS1mnISoG7U',
-      },
-      {
-        image: import('@/assets/favorites/videos/video-3.jpeg'),
-        title: "Why Isn't Functional Programming the Norm? - Richard Feldman",
-        url: 'https://youtu.be/QyJZzq0v7Z4',
+        image: import('@/assets/favorites/videos/just-javascript.jpg'),
+        title: 'Just JavaScript',
+        url: 'https://justjavascript.com/',
       },
     ],
   },
@@ -96,40 +74,34 @@ const favoritesSectionData = {
     title: 'Media I follow',
     data: [
       {
-        image: import('@/assets/favorites/media/media-1.jpeg'),
+        image: import('@/assets/favorites/media/fireship.jpeg'),
         title: 'Fireship.io',
         type: 'YouTube channel',
-        url: 'https://www.youtube.com/c/Fireship',
+        url: 'https://www.youtube.com/@Fireship',
       },
       {
-        image: import('@/assets/favorites/media/media-2.jpeg'),
-        title: 'Healthy Software Developer',
+        image: import('@/assets/favorites/media/matt_pocock.jpg'),
+        title: 'Matt Pocock',
         type: 'YouTube channel',
-        url: 'https://www.youtube.com/channel/UCfe_znKY1ukrqlGActlFmaQ',
+        url: 'https://www.youtube.com/@mattpocockuk',
       },
       {
-        image: import('@/assets/favorites/media/media-3.png'),
+        image: import('@/assets/favorites/media/net_ninja.jpg'),
+        title: 'Net Ninja',
+        type: 'YouTube channel',
+        url: 'https://www.youtube.com/@NetNinja',
+      },
+      {
+        image: import('@/assets/favorites/media/web_dev_simplified.jpg'),
+        title: 'Web Dev Simplified',
+        type: 'YouTube channel',
+        url: 'https://www.youtube.com/@WebDevSimplified',
+      },
+      {
+        image: import('@/assets/favorites/media/bytes.png'),
         title: 'Bytes',
         type: 'Newsletter',
         url: 'https://bytes.dev/',
-      },
-      {
-        image: import('@/assets/favorites/media/media-4.png'),
-        title: 'TypeScript Weekly',
-        type: 'Newsletter',
-        url: 'https://typescript-weekly.com/',
-      },
-      {
-        image: import('@/assets/favorites/media/media-5.jpeg'),
-        title: 'Front End Happy Hour',
-        type: 'Podcast',
-        url: 'https://www.frontendhappyhour.com/',
-      },
-      {
-        image: import('@/assets/favorites/media/media-6.webp'),
-        title: '.cult by Honeypot',
-        type: 'Blog',
-        url: 'https://cult.honeypot.io/',
       },
     ],
   },
