ba9e69c46984d0b644cd8de3802455073fd493dc
CatMonster <stefano.castiglia@protonmail.ch>
2023-08-30 08:22:35 +0200

    Hides testimonials

--- src/data/sections/index.ts
+++ src/data/sections/index.ts
@@ -6,7 +6,7 @@ import favoritesData from './favorites-section.data';
 import mainData from './main-section.data';
 import portfolioData from './portfolio-section.data';
 import skillsData from './skills-section.data';
-import testimonialsData from './testimonials-section.data';
+// import testimonialsData from './testimonials-section.data';
 
 export const sections = {
   main: mainData,
@@ -14,7 +14,7 @@ export const sections = {
   experience: experienceData,
   portfolio: portfolioData,
   education: educationData,
-  testimonials: testimonialsData,
+  // testimonials: testimonialsData,
   favorites: favoritesData,
 } as const satisfies ReadonlyDeep<Sections>;
 
